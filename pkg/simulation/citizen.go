02b3a66053559292f2f6fd2bef2cde69d9c93a7c
TheDonDope <idontreallyneedthese@gmail.com>
2020-01-31 20:07:43 +0100

    feat: add coordinates and reachability

--- pkg/simulation/citizen.go
+++ pkg/simulation/citizen.go
@@ -4,8 +4,16 @@ import (
 	"github.com/TheDonDope/govalues/pkg/politics"
 )
 
+// Coordinate represents the two dimensional position within the world
+type Coordinate struct {
+	X float64
+	Y float64
+}
+
 // Citizen represents a person in a world
 type Citizen struct {
-	Hitpoints int8
-	Ideology  politics.Ideology
+	Hitpoints  int8
+	Coordinate Coordinate
+	Ideology   politics.Ideology
+	// ContactedIdeologies []politics.Ideology     # List of ideologies which have already been in contact with
 }
--- pkg/simulation/world.go
+++ pkg/simulation/world.go
@@ -1,6 +1,18 @@
 package simulation
 
+import (
+	"math"
+)
+
+const reachable = 2
+
 // World represents a simulation container with multiple persons.
 type World struct {
 	Citizens []Citizen
+}
+
+// IsReachable returns if the given citizens are in reach for combat.
+func IsReachable(oneCitizen, anotherCitizen Citizen) bool {
+	overallDistance := math.Sqrt(math.Pow(anotherCitizen.Coordinate.X-oneCitizen.Coordinate.X, 2) + math.Pow(anotherCitizen.Coordinate.Y-oneCitizen.Coordinate.Y, 2))
+	return overallDistance <= reachable
 }
